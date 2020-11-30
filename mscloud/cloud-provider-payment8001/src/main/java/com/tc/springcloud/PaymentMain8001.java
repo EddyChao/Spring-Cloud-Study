8768aa5f1aae26586a33ecd3945240462b740eae
EddyChao <74349287+EddyChao@users.noreply.github.com>
2020-11-30 15:31:10 +0800

    搭建Spring Cloud环境

---
+++ mscloud/cloud-provider-payment8001/src/main/java/com/tc/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.tc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author v_ichaotang
+ * @Date 2020/11/30 15:27
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
